d9536dee015821f0800bf424370b6555e6ecf992
quankmksnb <quannguyenkm16122004@gmail.com>
2025-06-02 11:18:36 +0700

    add compoent

--- angular-basic/src/app/app.component.ts
+++ angular-basic/src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-basic';
+  // // text
+  // title = {
+  //   name: 'minh quan',
+  //   age: 20
+  // };
+
+  // // properties
+  // isDisabled = false;
+
+  // // attribute
+  // contentImg = "hello bà già";
+  nameBtn = 'Click tôi ';
+  ClickMessage = '';
+  handleClick() : void {
+    this.ClickMessage = 'Đã ấn cờ lích';
+  };
+  updateField() : void {
+    console.log('updateField');
+  };
+
+  bindingMessage = '';
 }
---
+++ angular-basic/src/app/shared/header-layout/header-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-layout',
+    standalone: true,
+    templateUrl: './header-layout.component.html',
+    styleUrls: ['./header-layout.component.css']
+})
+
+export class HeaderLayoutComponent {}
